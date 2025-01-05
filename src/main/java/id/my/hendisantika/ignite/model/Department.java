b54cf2bbfc9fa5151660667dd168ad6066f1c93d
hendisantika <hendisantika@yahoo.co.id>
2025-01-05 09:33:54 +0700

    Add Department Enum Model🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/ignite/model/Department.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.ignite.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ignite
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/01/25
+ * Time: 09.33
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Department {
+    HR, LEGAL, FINANCE
+}
