ffa345933f3e5b4711726e85b3621683138bced2
hendisantika <hendisantika@yahoo.co.id>
2025-01-05 09:29:35 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/ignite/SpringBootIgniteApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.ignite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIgniteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIgniteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/ignite/SpringBootIgniteApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.ignite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIgniteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
