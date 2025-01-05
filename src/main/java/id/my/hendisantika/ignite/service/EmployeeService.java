e6aa88a450a0c6a7c8d1962982cb369a09e249df
hendisantika <hendisantika@yahoo.co.id>
2025-01-05 09:38:44 +0700

    Add EmployeeService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/ignite/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.ignite.service;
+
+import id.my.hendisantika.ignite.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ignite
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/01/25
+ * Time: 09.38
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+}
