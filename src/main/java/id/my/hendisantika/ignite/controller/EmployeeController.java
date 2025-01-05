0fa90272a06839ff111dc0ab009657b5f4f52ad3
hendisantika <hendisantika@yahoo.co.id>
2025-01-05 09:40:51 +0700

    Add getEmployeesForDepartment in EmployeeController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/ignite/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/ignite/controller/EmployeeController.java
@@ -1,8 +1,14 @@
 package id.my.hendisantika.ignite.controller;
 
+import id.my.hendisantika.ignite.model.Department;
+import id.my.hendisantika.ignite.model.Employee;
 import id.my.hendisantika.ignite.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EmployeeController {
     private final EmployeeService employeeService;
+
+    @GetMapping(path = "/department/employees/{department}")
+    public List<Employee> getEmployeesForDepartment(@PathVariable("department") Department department) {
+        return employeeService.getEmployeesForDepartment(department);
+    }
 }
